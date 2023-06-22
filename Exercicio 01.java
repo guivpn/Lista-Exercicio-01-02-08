5673f6aa605207f6f345329fc626bbdd72cb3160
Guilherme <guilhermeviciusgv42@gmail.com>
2023-06-22 19:10:38 -0300

    Envio de atividade 01

---
+++ Exercicio 01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class main {
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("Alo mundo! ");
+
+  }
+}
