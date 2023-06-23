61fc308c70bfb49eb009ab5736b1bebeea1b6ed9
Guilherme <guilhermeviciusgv42@gmail.com>
2023-06-22 21:02:56 -0300

    Exercicio 01 lista 03

---
+++ Projeto 3 - Exercicio 01/src/Bola.java
@@ -0,0 +1,13 @@
+public class Bola {
+    public String cor;
+    public double circunferencia;
+    public double modelo;
+
+    public void trocarCor(String novaCor) {
+        cor = novaCor;
+    }
+
+    public String mostrarCor() {
+        return cor;
+    }
+}
